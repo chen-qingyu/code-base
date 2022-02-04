7fc63e0d0c20b748eabce53ef1f7864d54bb470f
chen-qingyu <236521827@qq.com>
2022-02-04 23:44:04 +0800

    update

--- TCP/client.py
+++ TCP/client.py
@@ -4,25 +4,33 @@ HOST = "127.0.0.1"
 PORT = 9999
 BUFFER_SIZE = 1024
 
-print("Welcome to the TCP simulation applet.")
-print("cmd:")
-print("  exit - Exit the client.")
-print("  shutdown - Shut down the server.")
-print("To IP %s, port %s" % (HOST, PORT))
-print()
-while True:
-    send_data = input("cmd: ")
-    if send_data.strip() == "":
-        continue
-    if send_data == "exit":
-        break
-    with socket.socket() as s:
-        try:
-            s.connect((HOST, PORT))
-        except socket.error:
-            print("Connection failed.\n")
+
+def main():
+    print("Welcome to the TCP simulation applet.")
+    print("cmd:")
+    print("  find <key> - Find the value for the given key.")
+    print("  write <key: value> - Write key-value pair.")
+    print("  exit - Exit the client.")
+    print("  shutdown - Shut down the server.")
+    print("To IP %s, port %s" % (HOST, PORT))
+    print()
+    while True:
+        send_data = input("cmd: ")
+        if send_data.strip() == "":
             continue
-        s.sendall(send_data.encode())
-        recv_data = s.recv(BUFFER_SIZE)
-        print(recv_data.decode(), "\n")
-print("Bye!")
+        if send_data == "exit":
+            break
+        with socket.socket() as s:
+            try:
+                s.connect((HOST, PORT))
+            except socket.error:
+                print("Connection failed.\n")
+                continue
+            s.sendall(send_data.encode())
+            recv_data = s.recv(BUFFER_SIZE)
+            print(recv_data.decode(), "\n")
+    print("Bye!")
+
+
+if __name__ == '__main__':
+    main()
--- TCP/server.py
+++ TCP/server.py
@@ -1,21 +1,70 @@
 import socket
+import os
 
 HOST = "127.0.0.1"
 PORT = 9999
 BUFFER_SIZE = 1024
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("Server start at IP %s, port %s\n" % (HOST, PORT))
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            recv_data = conn.recv(BUFFER_SIZE)
-            if (recv_data.decode() == "shutdown"):
-                print("Server shutdown.")
-                conn.sendall("Server shutdown.".encode())
-                break
-            print("From IP %s, port %s: %s" % (addr[0].strip("'"), addr[1], recv_data.decode()))
-            print()
-            conn.sendall("Server received.".encode())
+FILE_PATH = "./serverfile.txt"
+SPLIT = ": "
+
+
+def findData(key: str) -> str:
+    value = "Not found"
+    with open(FILE_PATH, "r") as fo:
+        for line in fo.readlines():
+            if line.split(SPLIT)[0] == key:
+                # Find value and remove the '\n' character
+                value = line.split(SPLIT)[1][:-1]
+    return value
+
+
+def writeData(key: str, value: str) -> str:
+    with open(FILE_PATH, "a") as fo:
+        fo.write(key + SPLIT + value + "\n")
+    return "Write data finished."
+
+
+def processCmd(cmd) -> str:
+    FIND = "find "
+    WRITE = "write "
+    ret = ""
+    if cmd[:len(FIND)] == FIND:
+        ret = findData(cmd[len(FIND):])
+    elif cmd[:len(WRITE)] == WRITE:
+        try:
+            key, value = cmd[len(WRITE):].split(SPLIT)
+            ret = writeData(key, value)
+        except Exception:
+            ret = "syntax: write <key: value>, for example: write foo: bar"
+    return ret
+
+
+def main():
+    if os.path.exists(FILE_PATH) == False:
+        open(FILE_PATH, "w").close()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print("Server start at IP %s, port %s\n" % (HOST, PORT))
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                recv_data = conn.recv(BUFFER_SIZE)
+                if (recv_data.decode() == "shutdown"):
+                    print("Server shutdown.")
+                    conn.sendall("Server shutdown.".encode())
+                    break
+                print("From IP %s, port %s: %s" %
+                      (addr[0].strip("'"), addr[1], recv_data.decode()))
+                print()
+                result = processCmd(recv_data.decode())
+                if result != "":
+                    conn.sendall(result.encode())
+                else:
+                    conn.sendall("Server received.".encode())
+
+
+if __name__ == '__main__':
+    main()
