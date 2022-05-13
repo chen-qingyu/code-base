d4684705a04a7e54afb2af68a9bb452e296f9e72
chen-qingyu <236521827@qq.com>
2022-05-13 23:07:02 +0800

    update

--- AutoGUI/臻臻.py
+++ AutoGUI/臻臻.py
@@ -9,10 +9,10 @@ pyautogui.click('start.png')
 pyautogui.move(100, -100)
 pyautogui.click()
 pyautogui.click('sublime.png')
-pyautogui.press(['z','f','z','f'])
+
+pyautogui.press(['z', 'f', 'z', 'f'])
 pyautogui.press('1')
 pyautogui.press('!')
-
-pyautogui.press(['z','f','z','f'])
+pyautogui.press(['z', 'f', 'z', 'f'])
 pyautogui.press('1')
 pyautogui.press('~')
