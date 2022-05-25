dc265d5b88dd1d303cde85048ea3399626d1faa1
chen-qingyu <236521827@qq.com>
2022-05-25 14:58:18 +0800

    update

--- fixed_point.py
+++ fixed_point.py
@@ -10,11 +10,15 @@ def bit_inverse(num: str) -> str:
     return ''.join(map(lambda c: "0" if c == "1" else "1", num))
 
 
-def plus_one(num: str) -> str:
+assert bit_inverse("0000") == "1111"
+assert bit_inverse("1010") == "0101"
+
+
+def bit_plus_one(num: str) -> str:
     """
     Binary number plus one.
-      plus_one("0000") -> "0001"
-      plus_one("1111") -> "0000"
+      bit_plus_one("0000") -> "0001"
+      bit_plus_one("1111") -> "0000"
 
     :param num: a binary number
     :return: binary number after adding one
@@ -24,6 +28,41 @@ def plus_one(num: str) -> str:
     while len(num) <= bits:  # 补零至原长度
         num = "0" + num
     return num[-bits:]  # 丢弃溢出位
+
+
+assert bit_plus_one("0000") == "0001"
+assert bit_plus_one("1111") == "0000"
+
+
+def binary_form(num: str, base=10) -> str:
+    """
+    To convert a number into its binary form.
+      binary_form("3") -> "+11"
+      binary_form("-3") -> "-11"
+      binary_form("11", 2) -> "+11"
+      binary_form("-11", 2) -> "-11"
+
+    :param num: a number
+    :param base: base system, 2 or 10
+    :return: binary form of num
+    """
+    sign_bit = '+' if num[0] != '-' else '-'
+    num = num.lstrip("+-")
+
+    if base == 10:
+        num = bin(eval(num)).removeprefix("0b")
+    elif base == 2:
+        pass
+    else:
+        raise RuntimeError("The base can only be 2 or 10")
+
+    return sign_bit + num
+
+
+assert binary_form("3") == "+11"
+assert binary_form("-3") == "-11"
+assert binary_form("11", 2) == "+11"
+assert binary_form("-11", 2) == "-11"
 
 
 def true_form(num: str, word_length=8, base=10) -> str:
@@ -42,139 +81,196 @@ def true_form(num: str, word_length=8, base=10) -> str:
     :param num: a number
     :param word_length: machine word length
     :param base: base system, 2 or 10
-    :return: num's true form
+    :return: true form of num
     """
-    sign_bit = '0' if num[0] != '-' else '1'
+    num = binary_form(num, base)
 
+    sign_bit = '0' if num[0] == '+' else '1'
     num = num.lstrip("+-0")
-
-    if base == 10:
-        num = bin(eval(num)).removeprefix("0b")
-    elif base == 2:
-        pass
-    else:
-        raise RuntimeError("The base can only be 2 or 10")
 
     if len(num) > word_length - 1:
         raise ValueError("Machine word length is not enough")
 
+    # 原码等于二进制符号位 +: 0, -: 1，数值位不变，一共字长位
     while len(num) != word_length - 1:
         num = "0" + num
 
     return sign_bit + num
 
 
-def complement_form(num: str) -> str:
-    """
-    Change the true form of a number into the complement form.
+assert true_form("0", 4) == "0000"
+assert true_form("7", 4) == "0111"
+try:
+    true_form("8", 4)
+except ValueError as e:
+    assert e.args[0] == "Machine word length is not enough"
+assert true_form("8") == "00001000"
+assert true_form("-0", 4) == "1000"
+assert true_form("-7", 4) == "1111"
+assert true_form("101", 4, 2) == "0101"
+assert true_form("00000101", 4, 2) == "0101"
+assert true_form("-0101", 4, 2) == "1101"
 
-    :param num: a binary number in its true form
-    :return: a binary number in its complement form
-    """
 
-    if num[0] == "0":  # 正数
-        inverse = num  # 反码等于原码
-        complement = inverse  # 补码等于反码
-    else:  # 负数
-        inverse = "1" + bit_inverse(num[1:])  # 反码等于原码符号位不变，数值位按位取反
-        complement = plus_one(inverse)  # 补码等于反码加一
+def inverse_form(num: str, word_length=8, base=10) -> str:
+    num = true_form(num, word_length, base)
+
+    if num[0] == "0":  # 正数反码等于原码
+        inverse = num
+    else:  # 负数反码等于原码符号位不变，数值位取反
+        inverse = num[0] + bit_inverse(num[1:])
+
+    return inverse
+
+
+assert inverse_form("0") == "00000000"
+assert inverse_form("-0") == "11111111"
+assert inverse_form("1") == "00000001"
+assert inverse_form("-1") == "11111110"
+
+
+def complement_form(num: str, word_length=8, base=10) -> str:
+    num = inverse_form(num, word_length, base)
+
+    if num[0] == "0":  # 正数补码等于反码
+        complement = num
+    else:  # 负数补码等于反码加一
+        complement = bit_plus_one(num)
 
     return complement
 
 
-def machine_representation(x: str, word_length: int = 8) -> dict:
-    d = {"真值": x}
+assert complement_form("0") == "00000000"
+assert complement_form("-0") == "00000000"
+assert complement_form("-1") == "11111111"
+assert complement_form("+5") == "00000101"
 
-    # 二进制
-    x_bin = bin(eval(x)).replace("0b", "")
-    x_bin = ('+' + x_bin if x[0] != '-' else x_bin)
-    d["二进制"] = x_bin
 
-    # 原码
-    # 原码等于二进制符号位 +: 0, -: 1，数值位不变，一共字长位
-    true_form = x_bin[0] + ("0" * (word_length - 1 - len(x_bin[1:]))) + x_bin[1:]
-    true_form = ("0" if true_form[0] == "+" else "1") + true_form[1:]
-    d["原码"] = true_form
+def shift_form(num: str, word_length=8, base=10) -> str:
+    num = complement_form(num, word_length, base)
 
-    # 反码
-    if true_form[0] == "0":  # 正数反码等于原码
-        inverse = true_form
-    else:  # 负数反码等于原码符号位不变，数值位取反
-        inverse = "1" + ''.join(map(lambda c: "0" if c == "1" else "1", true_form[1:]))
-    d["反码"] = inverse
-
-    # 补码
-    if inverse[0] == "0":  # 正数补码等于反码
-        complement = inverse
-    else:  # 负数补码等于反码加一
-        complement = bin(eval("0b" + inverse) + 1)[-8:]
-    d["补码"] = complement
-
-    # 移码
     # 移码等于补码符号位取反
-    shift = ("0" if complement[0] == "1" else "1") + complement[1:]
-    d["移码"] = shift
+    shift = ("0" if num[0] == "1" else "1") + num[1:]
+
+    return shift
+
+
+assert shift_form("0") == "10000000"
+assert shift_form("-0") == "10000000"
+assert shift_form("-1") == "01111111"
+assert shift_form("+5") == "10000101"
+
+
+def machine_representation(num: str, word_length=8, base=10) -> dict:
+    d = {
+        "真值": num,
+        "二进制": binary_form(num, base),
+        "原码": true_form(num, word_length, base),
+        "反码": inverse_form(num, word_length, base),
+        "补码": complement_form(num, word_length, base),
+        "移码": shift_form(num, word_length, base)
+    }
 
     return d
 
 
-# for X in ["-5", "-2", "-1", "-0", "0", "1", "2", "5"]:
-#     for desc, code in machine_representation(X).items():
+# for number in ["-5", "-2", "-1", "-0", "0", "1", "2", "5"]:
+#     for desc, code in machine_representation(number).items():
 #         print(desc + ": " + code[:4] + " " + code[4:])
 #     print()
 
+def bit_add(x: str, y: str) -> str:
+    if len(x) != len(y):
+        raise RuntimeError("x's and y's bits are required to be equal")
 
-def booth(x: str, y: str) -> str:
-    # 机器字长
-    word_len = 5
-    # 数值位长
-    n = word_len - 1
+    word_length = len(x)
+    num = bin(eval("0b" + x) + eval("0b" + y)).removeprefix("0b")[-word_length:]
+    while len(num) != word_length:
+        num = "0" + num
+    return num
 
-    # x = x.replace('.', '')
-    # y = y.replace('.', '')
 
-    if x[0] != '-':  # 正数
-        mx = "11" + bin(eval("0b" + (''.join(map(lambda c: "0" if c == "1" else "1", (x.split(".")[1]))))) + 1).replace(
-            "0b", "")
-        x = "00" + x.split(".")[1]
-    else:
-        mx = "00" + x.split(".")[1]
-        x = bin(eval("0b" + ''.join(map(lambda c: "0" if c == "1" else "1", (x.split(".")[1])))) + 1).replace("0b", "")
-        while len(x) != n:
-            x = "0" + x
-        x = "11" + x
+def booth(x: str, y: str, word_length=5, base=2, print_step=False) -> str:
+    """
+    Booth algorithm.
+      booth("-0.1101", "0.1011") -> "-0.10001111"
 
-    if y[0] != '-':
-        y = "0" + y.split(".")[1] + "0"
-    else:
-        y = "1" + bin(eval("0b" + (''.join(map(lambda c: "0" if c == "1" else "1", (y.split(".")[1]))))) + 1)[-n:] + "0"
+    :param x: a number
+    :param y: a number
+    :param word_length: machine word length
+    :param base: base system, 2 or 10
+    :param print_step: whether print steps
+    :return: binary representation of the result of multiplication
+    """
+    is_decimal = False
+    if '.' in x and '.' in y:
+        is_decimal = True
+        x = x.replace('.', '')
+        y = y.replace('.', '')
+
+    x = complement_form(x, word_length, base)
+    x = x[0] + x
+
+    mx = bit_plus_one(bit_inverse(x))
+
+    y = complement_form(y, word_length, base)
+    y = y + "0"
+
+    if print_step:
+        print(f"[x]双符号补={x}, [-x]双符号补={mx}, [y]补添零={y}")
 
     # 高位部分积
-    hp = "0" * (n + 2)
+    hp = "0" * (word_length + 1)
 
     # 低位部分积
     lp = ""
 
-    for i in range(word_len):
+    for i in range(word_length):
+        if print_step:
+            print(f"step {i + 1}: hp='{hp}', lp='{lp}', y[-2:]='{y[-2:]}', "
+                  + ("adding & shifting..." if i != word_length - 1 else "adding..."))
+
         if y[-2:] == "00" or y[-2:] == "11":
             pass
         elif y[-2:] == "01":
-            hp = bin(eval("0b" + hp) + eval("0b" + x))[2:][-(n + 2):]
+            hp = bit_add(hp, x)
         elif y[-2:] == "10":
-            hp = bin(eval("0b" + hp) + eval("0b" + mx))[2:][-(n + 2):]
+            hp = bit_add(hp, mx)
 
-        while len(hp) != n + 2:
-            hp = "0" + hp
-
-        print(hp, "---", lp)
-
-        if i == word_len - 1:
+        if i == word_length - 1:
+            if print_step:
+                print(f"final : hp='{hp}', lp='{lp}'")
             break
 
+        # 部分积右移一位
         lp = hp[-1] + lp
-        hp = (hp[0] + hp)[:n + 2]
+        hp = (hp[0] + hp)[:word_length + 1]
+        # y右移一位
         y = y[:-1]
 
-    return hp + lp
+    # 乘积的补码
+    complement = hp[1:] + lp
+    is_positive_or_0 = True if hp[0] == '0' else False
 
-# print(booth("-0.1101", "0.1011"))
+    # 从补码到原码
+    if is_positive_or_0:
+        origin = complement
+    else:
+        origin = complement[0] + bit_plus_one(bit_inverse(complement[1:]))
+
+    # 处理小数，取绝对值
+    if is_decimal:
+        num = "0." + origin[1:]
+    else:
+        num = origin[1:]
+
+    # 添加符号
+    return ('+' if is_positive_or_0 else '-') + num
+
+
+assert booth("-0.1101", "0.1011", print_step=False) == "-0.10001111"
+assert booth("2", "4", base=10, print_step=False) == "+00001000"
+assert booth("-2", "4", base=10, print_step=False) == "-00001000"
+assert booth("2", "-4", base=10, print_step=False) == "-00001000"
+assert booth("-2", "-4", base=10, print_step=False) == "+00001000"
+assert booth("1", "1", base=10, print_step=False) == "+00000001"
