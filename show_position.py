fc6c7cd59cf2b52ee83f2dd5392772890ce84492
chen-qingyu <236521827@qq.com>
2023-03-28 01:37:17 +0800

    refine show_position.py

--- show_position.py
+++ show_position.py
@@ -2,10 +2,10 @@ import pyautogui
 import time
 
 print("Press Ctrl-C to quit.")
-while True:
-    try:
+try:
+    while True:
         x, y = pyautogui.position()
         print(f"X: {x:4d} Y: {y:4d}", end='\r')
         time.sleep(0.01)  # 实测加上这一行几乎不影响使用但CPU占用率从30%左右降到了10%左右
-    except KeyboardInterrupt:
-        break
+except KeyboardInterrupt:
+    pass
