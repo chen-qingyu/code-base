e4477d4f1c6aa6a0224d6d96d031aa1e6e03f2f6
chen-qingyu <236521827@qq.com>
2023-04-22 17:11:41 +0800

    refine batch_install.py

--- batch_install.py
+++ batch_install.py
@@ -7,13 +7,6 @@
 #   - upgrade libraries
 
 import os
-import colorama
-
-colorama.init(autoreset=True)
-
-COLOR_START = colorama.Fore.BLUE + colorama.Style.BRIGHT
-COLOR_INFO = colorama.Fore.CYAN + colorama.Style.BRIGHT
-COLOR_FINISH = colorama.Fore.GREEN + colorama.Style.BRIGHT
 
 LIBS = (
     "pip",
@@ -61,9 +54,9 @@ LIBS = (
 
 if __name__ == '__main__':
     for i in range(len(LIBS)):
-        print(COLOR_START + f"({i + 1}/{len(LIBS)}) Start install/upgrade {LIBS[i]}")
+        print(f"({i + 1}/{len(LIBS)}) Start install/upgrade {LIBS[i]}")
         os.system("python -m pip install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple/ " + LIBS[i])
         print()
 
-    print(COLOR_FINISH + f"A total of {len(LIBS)} libraries have been installed/upgraded.")
+    print(f"A total of {len(LIBS)} libraries have been installed/upgraded.")
     input()
