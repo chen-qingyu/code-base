738cc904efde255e8801175d3383aba1552d4fbd
chen-qingyu <236521827@qq.com>
2023-09-21 08:07:31 +0800

    大幅度重构，使能无依赖初始化

--- batch_pip.py
+++ batch_pip.py
@@ -4,16 +4,14 @@
 # CreateDate: 2020.10.04
 
 import os
+import importlib
 
 LIBS = (
-    "pip",
-    "setuptools",
-    "wheel",
-
-    "numpy",
-    "matplotlib",
+    # "numpy",  # the latest version conflicts with tensorflow
     "scipy",
     "pandas",
+
+    "matplotlib",
     "pillow",  # PIL
 
     "pyinstaller",
@@ -27,7 +25,7 @@ LIBS = (
     "requests",
     "beautifulsoup4",  # bs4
 
-    "opencv-python",  # cv2, version conflict with easyocr
+    "opencv-python",  # cv2, name conflict with easyocr's dep: opencv-python-headless
 
     "tensorflow",
 
@@ -49,11 +47,48 @@ LIBS = (
     "djlint",
 )
 
-if __name__ == '__main__':
+
+def main():
+    init()
+    process()
+
+
+def init():
+    print("Start init.\n")
+
+    print("Configuring mirror source.")
+    os.system("python -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple")
+    print("Configured mirror source.\n")
+
+    print("Upgrading pip tools.")
+    os.system("python -m pip install --upgrade pip setuptools wheel")
+    print("Upgraded pip tools.\n")
+
+    print("Loading colorama.")
+    if importlib.util.find_spec("colorama") is None:
+        os.system("python -m pip install colorama")
+    colorama = importlib.import_module("colorama")
+    colorama.init(autoreset=True)
+    global COLOR_START, COLOR_INFO, COLOR_FINISH, COLOR_ERROR
+    COLOR_START = colorama.Fore.BLUE + colorama.Style.BRIGHT
+    COLOR_INFO = colorama.Fore.CYAN + colorama.Style.BRIGHT
+    COLOR_FINISH = colorama.Fore.GREEN + colorama.Style.BRIGHT
+    COLOR_ERROR = colorama.Fore.RED + colorama.Style.BRIGHT
+    print(COLOR_INFO + "Loaded colorama.\n")
+
+    print(COLOR_FINISH + "Finish init.\n")
+
+
+def process():
+    print(COLOR_START + "Start install/upgrade libraries.\n")
+
     for i in range(len(LIBS)):
-        print(f"({i + 1}/{len(LIBS)}) Start install/upgrade {LIBS[i]}")
-        os.system("python -m pip install --upgrade -i https://mirrors.aliyun.com/pypi/simple/ " + LIBS[i])
+        print(COLOR_INFO + f"({i + 1}/{len(LIBS)}) installing/upgrading {LIBS[i]}")
+        os.system("python -m pip install --upgrade " + LIBS[i])
         print()
 
-    print(f"A total of {len(LIBS)} libraries have been installed/upgraded.")
-    input()
+    print(COLOR_FINISH + f"A total of {len(LIBS)} libraries have been installed/upgraded.")
+
+
+if __name__ == '__main__':
+    main()
