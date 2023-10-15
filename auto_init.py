b856a4f098ffe75ff6c67649cf79327c40703aaa
chen-qingyu <236521827@qq.com>
2023-10-15 22:26:07 +0800

    将软件和Python包都用toml进行配置

--- auto_init.py
+++ auto_init.py
@@ -7,9 +7,13 @@ import os
 import platform
 import webbrowser
 import importlib
+import tomllib
+
+with open('auto_init.toml', 'rb') as f:
+    data = tomllib.load(f)
 
 # install/upgrade libraries and clean cache
-os.system('python -m pip install --upgrade --index-url https://pypi.tuna.tsinghua.edu.cn/simple --requirement ./auto_init_requirements.txt')
+os.system(f'python -m pip install --upgrade --index-url https://pypi.tuna.tsinghua.edu.cn/simple {' '.join(data['libraries'])}')
 os.system("python -m pip cache purge")
 
 # dynamically loading third-party libraries
@@ -23,46 +27,6 @@ COLOR_START = colorama.Fore.BLUE + colorama.Style.BRIGHT
 COLOR_INFO = colorama.Fore.CYAN + colorama.Style.BRIGHT
 COLOR_FINISH = colorama.Fore.GREEN + colorama.Style.BRIGHT
 COLOR_ERROR = colorama.Fore.RED + colorama.Style.BRIGHT
-
-# software information keys
-NAME = 'name'  # software name, use the full name in winget if method is USE_WINGET
-METHOD = 'method'  # how to install the software
-DOWNLOAD_URL = 'download url'  # if automatic download, the download address
-INSTALL_ARGS = 'install args'  # if automatic install, the install arguments
-DOWNLOAD_LINK = 'download link'  # if manual download and install, the download link
-
-# METHOD: how to automatically download and install softwares
-MANUAL = 0
-AUTO_DOWNLOAD = 1
-AUTO_DOWNLOAD_INSTALL = 2
-USE_WINGET = 3
-
-SOFTWARES = (
-    {
-        NAME: 'Qt',
-        METHOD: AUTO_DOWNLOAD_INSTALL,
-        DOWNLOAD_URL: 'https://mirror.nju.edu.cn/qt/official_releases/online_installers/qt-unified-windows-x64-online.exe',
-        INSTALL_ARGS: '--mirror https://mirror.nju.edu.cn/qt',
-    },
-
-    {
-        NAME: 'Sublime Text',
-        METHOD: MANUAL,
-        DOWNLOAD_LINK: 'https://www.sublimetext.com/download_thanks?target=win-x64',
-    },
-
-    {
-        NAME: 'Bandizip',
-        METHOD: MANUAL,
-        DOWNLOAD_LINK: 'https://www.bandisoft.com/bandizip/dl.php?web',
-    },
-
-    {
-        NAME: 'Android Studio',
-        METHOD: MANUAL,
-        DOWNLOAD_LINK: 'https://developer.android.com/studio',
-    },
-)
 
 DOWNLOADS = './Downloads/'
 
@@ -87,44 +51,44 @@ def init():
 
 
 def process():
-    for i, software in zip(range(len(SOFTWARES)), SOFTWARES):
-        print(COLOR_START + f"({i + 1}/{len(SOFTWARES)}) Start download/install {software[NAME]}...")
+    for i, software in zip(range(len(data['softwares'])), data['softwares']):
+        print(COLOR_START + f"({i + 1}/{len(data['softwares'])}) Start download/install {software['name']}...")
 
-        if software[METHOD] == USE_WINGET:
-            os.system('winget install ' + software[NAME])
-        elif software[METHOD] == AUTO_DOWNLOAD_INSTALL:
+        if software['method'] == 'use_winget':
+            os.system('winget install ' + software['name'])
+        elif software['method'] == 'auto_download_install':
             download(software)
             install(software)
-        elif software[METHOD] == AUTO_DOWNLOAD:
+        elif software['method'] == 'auto_download':
             download(software)
-            input(COLOR_INFO + f"Please install {software[NAME]} manually.")
-        elif software[METHOD] == MANUAL:
-            webbrowser.open(software[DOWNLOAD_LINK])
-            input(COLOR_INFO + f"Please download and install {software[NAME]} manually.")
+            input(COLOR_INFO + f"Please install {software['name']} manually.")
+        elif software['method'] == 'manual':
+            webbrowser.open(software['download_link'])
+            input(COLOR_INFO + f"Please download and install {software['name']} manually.")
         else:
             print(COLOR_ERROR + "ERROR: Wrong method.")
 
-        print(COLOR_FINISH + f"Finish download/install {software[NAME]}.\n")
+        print(COLOR_FINISH + f"Finish download/install {software['name']}.\n")
 
 
 def download(software: dict):
-    print(COLOR_INFO + f"Downloading {software[NAME]}...")
-    file_name = software[DOWNLOAD_URL].split('/')[-1]
-    response = requests.get(software[DOWNLOAD_URL], stream=True)
+    print(COLOR_INFO + f"Downloading {software['name']}...")
+    file_name = software['download_url'].split('/')[-1]
+    response = requests.get(software['download_url'], stream=True)
     length = int(response.headers.get('content-length', 0))
     with open(DOWNLOADS + file_name, 'wb') as fo, tqdm.tqdm(
             desc=file_name, total=length, unit='iB', unit_scale=True, unit_divisor=1024) as bar:
         for data in response.iter_content(chunk_size=1024):
             size = fo.write(data)
             bar.update(size)
-    print(COLOR_INFO + f"Downloaded {software[NAME]}.")
+    print(COLOR_INFO + f"Downloaded {software['name']}.")
 
 
 def install(software: dict):
-    print(COLOR_INFO + f"Installing {software[NAME]}...")
-    file_name = software[DOWNLOAD_URL].split('/')[-1]
-    os.system('PowerShell ' + DOWNLOADS + file_name + ' ' + software[INSTALL_ARGS])
-    print(COLOR_INFO + f"Installed {software[NAME]}.")
+    print(COLOR_INFO + f"Installing {software['name']}...")
+    file_name = software['download_url'].split('/')[-1]
+    os.system('PowerShell ' + DOWNLOADS + file_name + ' ' + software['install_args'])
+    print(COLOR_INFO + f"Installed {software['name']}.")
 
 
 if __name__ == '__main__':
