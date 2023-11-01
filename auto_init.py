3e62910c1a8403140029609850564029bad00062
chen-qingyu <236521827@qq.com>
2023-11-01 17:21:39 +0800

    auto_init.py: 命令行参数化；重构动态加载第三方库

--- auto_init.py
+++ auto_init.py
@@ -8,51 +8,80 @@ import platform
 import webbrowser
 import importlib
 import tomllib
+import argparse
 
 with open('auto_init.toml', 'rb') as f:
     DATA = tomllib.load(f)
     SIZE = len(DATA['softwares'])
 
-# install/upgrade libraries and clean cache
-os.system(f'python -m pip install --upgrade --index-url https://pypi.tuna.tsinghua.edu.cn/simple {' '.join(DATA['libraries'])}')
-os.system('python -m pip cache purge')
-
-# dynamically loading third-party libraries
-colorama = importlib.import_module('colorama')
-requests = importlib.import_module('requests')
-tqdm = importlib.import_module('tqdm')
-
-colorama.init(autoreset=True)
-
-COLOR_START = colorama.Fore.BLUE + colorama.Style.BRIGHT
-COLOR_INFO = colorama.Fore.CYAN + colorama.Style.BRIGHT
-COLOR_FINISH = colorama.Fore.GREEN + colorama.Style.BRIGHT
-COLOR_ERROR = colorama.Fore.RED + colorama.Style.BRIGHT
-
 DOWNLOAD = './Download/'
 
 
 def main():
-    init()
-    process()
+    # parse command
+    parser = argparse.ArgumentParser(prog="auto_init", description="Python3 script for automating initialize the system environment.")
+    parser.add_argument("action", type=str, help="action", choices=['lib', 'pkg', 'all'])
+    parser.add_argument("name", type=str, help="Python library name or software package name", nargs='?', default='')
+    args = parser.parse_args()
 
+    global COLOR_START, COLOR_INFO, COLOR_FINISH, COLOR_ERROR
+    COLOR_START, COLOR_INFO, COLOR_FINISH, COLOR_ERROR = '', '', '', ''
 
-def init():
-    print(COLOR_START + "Start init.")
+    print("Start upgrade pip tools.")
+    lib('pip setuptools wheel')
+    print("Finish upgrade pip tools.")
 
+    print("Start dynamically load third-party libraries.")
+    lib('colorama requests tqdm')
+    global colorama, requests, tqdm
+    colorama = importlib.import_module('colorama')
+    requests = importlib.import_module('requests')
+    tqdm = importlib.import_module('tqdm')
+    print("Finish dynamically load third-party libraries.")
+
+    print("Start colorize.")
+    colorama.init(autoreset=True)
+
+    COLOR_START = colorama.Fore.BLUE + colorama.Style.BRIGHT
+    COLOR_INFO = colorama.Fore.CYAN + colorama.Style.BRIGHT
+    COLOR_FINISH = colorama.Fore.GREEN + colorama.Style.BRIGHT
+    COLOR_ERROR = colorama.Fore.RED + colorama.Style.BRIGHT
+    print(COLOR_FINISH + "Finish colorize.")
+
+    print(COLOR_START + "Checking platform...")
     if platform.system() != 'Windows':
         print(COLOR_ERROR + "This script currently only supports the Windows platform.\n")
         exit(-1)
+    print(COLOR_FINISH + "OK.")
 
+    print(COLOR_START + "Creating download folder...")
     if not os.path.exists(DOWNLOAD):
-        print(COLOR_INFO + f"Creates downloads folder: {DOWNLOAD}")
         os.mkdir(DOWNLOAD)
+        print(COLOR_INFO + f"Created: {DOWNLOAD}")
+    print(COLOR_FINISH + "OK.")
 
-    print(COLOR_FINISH + "Finish init.\n")
+    # process command
+    if args.action == 'lib':
+        lib(args.name)
+    elif args.action == 'pkg':
+        pkg(args.name)
+    else:
+        lib(' '.join(DATA['libraries']))
+        pkg('')
 
 
-def process():
+def lib(name: str):
+    print(COLOR_START + f"Start install/upgrade libraries: {name}")
+    os.system(f'python -m pip install --upgrade --index-url https://pypi.tuna.tsinghua.edu.cn/simple {name}')
+    os.system('python -m pip cache purge')
+    print(COLOR_FINISH + f"Finish install/upgrade libraries: {name}")
+
+
+def pkg(name: str):
     for i, software in zip(range(SIZE), DATA['softwares']):
+        if name != '' and software['name'].lower() != name.lower():
+            continue
+
         print(COLOR_START + f"({i + 1}/{SIZE}) Start download/install {software['name']}...")
 
         if software['method'] == 'use_winget':
