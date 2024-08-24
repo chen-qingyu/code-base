ba98c019fbbcd21ff2bb5fb62b41a8095306582d
chen-qingyu <236521827@qq.com>
2024-08-24 08:37:06 +0800

    fix: 处理负数

--- src/main.rs
+++ src/main.rs
@@ -46,36 +46,34 @@ fn fraction_to_decimal(line: &str) -> String {
 
 fn decimal_to_fraction(line: &str) -> String {
     let re_dec = Regex::new(r"^([-+]?\d+)\.?(\d+)?~?(\d+)?$").unwrap();
-
     let caps = re_dec.captures(line).unwrap();
-    let integral = caps[1].parse::<i32>().unwrap();
+    let integral = caps[1].parse::<i32>().unwrap().abs();
     let decimal = caps.get(2).map(|x| x.as_str().parse::<i32>().unwrap());
     let cyclic = caps.get(3).map(|x| x.as_str().parse::<i32>().unwrap());
+    let is_negative = caps[1].contains('-');
 
-    match (integral, decimal, cyclic) {
+    let f = match (integral, decimal, cyclic) {
         // 整数
-        (i, None, None) => format!("{}", Fraction::from(i)),
+        (i, None, None) => Fraction::from(i),
         // 非循环小数
         (i, Some(d), None) => {
             let decimal_len = caps[2].len() as u32;
-            format!("{}", Fraction::from((i * i32::pow(10, decimal_len) + d, i32::pow(10, decimal_len))))
+            Fraction::from((i * i32::pow(10, decimal_len) + d, i32::pow(10, decimal_len)))
         }
         // 循环小数-无非循环部分
         (i, None, Some(c)) => {
             let cyclic_len = caps[3].len() as u32;
-            format!("{}", Fraction::from(i) + Fraction::from((c, i32::pow(10, cyclic_len) - 1)))
+            Fraction::from(i) + Fraction::from((c, i32::pow(10, cyclic_len) - 1))
         }
         // 循环小数-有非循环部分
         (i, Some(d), Some(c)) => {
             let decimal_len = caps[2].len() as u32;
             let cyclic_len = caps[3].len() as u32;
-            format!(
-                "{}",
-                Fraction::from((i * i32::pow(10, decimal_len) + d, i32::pow(10, decimal_len)))
-                    + Fraction::from((c, (i32::pow(10, cyclic_len) - 1) * i32::pow(10, decimal_len)))
-            )
+            Fraction::from((i * i32::pow(10, decimal_len) + d, i32::pow(10, decimal_len)))
+                + Fraction::from((c, (i32::pow(10, cyclic_len) - 1) * i32::pow(10, decimal_len)))
         }
-    }
+    };
+    format!("{}", if is_negative { -f } else { f })
 }
 
 /// a: 被除数, b: 除数, 返回(循环节起始位置, 循环节长度)
@@ -119,6 +117,11 @@ mod tests {
         assert_eq!(fraction_to_decimal("123/1"), "123");
         assert_eq!(fraction_to_decimal("187/1665"), "0.1123123123...");
         assert_eq!(fraction_to_decimal("61111/4950"), "12.34565656...");
+
+        assert_eq!(fraction_to_decimal("-1/-1"), "1");
+        assert_eq!(fraction_to_decimal("19/-10"), "-1.9");
+        assert_eq!(fraction_to_decimal("-1/3"), "-0.333...");
+        assert_eq!(fraction_to_decimal("1/-9"), "-0.111...");
     }
 
     #[test]
@@ -137,5 +140,11 @@ mod tests {
         assert_eq!(decimal_to_fraction("0.~375"), "125/333"); // 0.375375375...
         assert_eq!(decimal_to_fraction("4.~518"), "122/27"); // 4.518518518...
         assert_eq!(decimal_to_fraction("0.~9"), "1"); // 0.999... = 1
+
+        assert_eq!(decimal_to_fraction("-1"), "-1");
+        assert_eq!(decimal_to_fraction("-0.~1"), "-1/9");
+        assert_eq!(decimal_to_fraction("-1.9"), "-19/10");
+        assert_eq!(decimal_to_fraction("-1.~9"), "-2");
+        assert_eq!(decimal_to_fraction("-1.1~9"), "-6/5");
     }
 }
