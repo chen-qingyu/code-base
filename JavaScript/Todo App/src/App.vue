e8277f4eae3d0107ca94e0cbbb5d1b3ee5517b0c
chen-qingyu <chen_qingyu@qq.com>
2025-01-20 01:26:22 +0800

    js: finish todo app

--- JavaScript/Todo App/src/App.vue
+++ JavaScript/Todo App/src/App.vue
@@ -1,47 +1,89 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const input = ref('')
+const list = ref([])
+
+function add() {
+  if (input.value === '') {
+    return
+  }
+
+  list.value.push({
+    value: input.value,
+    isCompleted: false,
+  })
+
+  input.value = ''
+}
+
+function del(index) {
+  list.value.splice(index, 1)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <h1 class="title">Todo App</h1>
+  <div class="input-form">
+    <input class="inpput-str" v-model="input" type="text" placeholder="请输入待办事项..." />
+    <button class="input-btn" @click="add">添加</button>
+  </div>
+  <div
+    v-for="(item, index) in list"
+    :key="index"
+    :class="['items', item.isCompleted ? 'completed' : 'uncompleted']"
+  >
+    <div>
+      <input v-model="item.isCompleted" type="checkbox" />
+      <span class="item-str">{{ item.value }}</span>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div><button @click="del(index)">删除</button></div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.title {
+  text-align: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.input-form {
+  display: flex;
+  margin: 30px auto 30px auto;
+  width: 60%;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.input-btn {
+  width: 20%;
+  height: 54px;
+  border-radius: 0 10px 10px 0;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.inpput-str {
+  width: 80%;
+  height: 50px;
+  padding: 0 15px;
+  border-radius: 10px 0 0 10px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.items {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 60%;
+  height: 50px;
+  margin: 8px auto;
+  padding: 16px;
+  box-sizing: border-box;
+  border-radius: 10px;
+  box-shadow: 0 4px 10px gray;
+}
+
+.item-str {
+  margin-left: 10px;
+}
+
+.completed {
+  text-decoration: line-through;
+  opacity: 0.4;
 }
 </style>
